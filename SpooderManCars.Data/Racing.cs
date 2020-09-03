ec86e4b343acf10be9c527b76b83683a9c397386
mitch reed <reed.mitch@live.com>
2020-09-03 11:20:06 -0400

    Added RaceTeam Info

---
+++ SpooderManCars.Data/Racing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpooderManCars.Data
+{
+    public class Racing
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Manufacturer))]
+        public int ManufacturerID { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; }
+        public string TeamName { get; set; }
+        public string BasedOutOF { get; set; }
+        public List<int> Victories { get; set; } = new List<int>();
+        public string Drivers { get; set; }
+        public RaceEvent RaceEvent { get; set; }
+        
+    }
+
+    public enum RaceEvent
+    {
+        F1,
+        Nascar,
+        IndyCar,
+        DragRacing,
+        SportsCarChampionship,
+    }
+
+}
