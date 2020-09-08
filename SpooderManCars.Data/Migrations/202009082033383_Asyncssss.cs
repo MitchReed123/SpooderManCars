4a6e77b5cb44ac147cbcfe9d853e1abf1751cb4b
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2020-09-08 16:34:33 -0400

    Added Async conversion 2

---
+++ SpooderManCars.Data/Migrations/202009082033383_Asyncssss.cs
@@ -0,0 +1,16 @@
+namespace SpooderManCars.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Asyncssss : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
