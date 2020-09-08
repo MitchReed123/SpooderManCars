8e56a1a26f0fd5b278e8d1f0b9e048b5fad6e0d5
Chris Blinde <chrisblinde@gmail.com>
2020-09-08 11:54:17 -0400

    added Put

--- SpooderManCars.Services/ManufacturerService.cs
+++ SpooderManCars.Services/ManufacturerService.cs
@@ -1,6 +1,8 @@
 ﻿using SpooderManCars.Data;
+using SpooderManCars.Models.ManufacturerModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,43 +11,94 @@ namespace SpooderManCars.Services
 {
     public class ManufacturerService
     {
-            public bool CreateManufacturer(Manufacturer model)
+        public bool CreateManufacturer(Manufacturer model)
+        {
+            var entity = new Manufacturer()
             {
-                var entity = new Manufacturer()
-                {
-                    CompanyName = model.CompanyName,
-                    Locations = model.Locations,
-                    Founded = model.Founded
-                };
-                using (var ctx = new ApplicationDbContext())
-                {
-                    ctx.Manufacturers.Add(entity);
+                CompanyName = model.CompanyName,
+                Locations = model.Locations,
+                Founded = model.Founded
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Manufacturers.Add(entity);
 
-                    return ctx.SaveChanges() == 1;
-                }
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public List<ManufacturerListItem> GetManufacturer()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Manufacturers.Select(
+                    e =>
+                    new ManufacturerListItem
+                    {
+                        Id = e.Id,
+                        CompanyName = e.CompanyName,
+                        Locations = e.Locations,
+                        Cars = e.Cars,
+                        Founded = e.Founded
+                    }
+                    );
+                return query.ToList();
+            }
+        }
+
+        public ManufacturerDetail GetManufacturerById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Manufacturers
+                        .Single(e => e.Id == id);
+                return
+                    new ManufacturerDetail
+                    {
+                        Id = entity.Id,
+                        CompanyName = entity.CompanyName
+                    };
+            }
+        }
+
+
+        public bool UpdateManufacturer(ManufacturerEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Manufacturers
+                        .Single(e => e.Id == model.Id && e.CompanyName == model.CompanyName);
+
+
+                entity.Id = model.Id;
+                entity.CompanyName = model.CompanyName;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteManufacturer(int Id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Manufacturers
+                    .Single(e => e.Id == Id && e.CompanyName == e.CompanyName);
+                ctx.Manufacturers.Remove(entity);
+                return ctx.SaveChanges() == 1;
             }
 
-            public List<Manufacturer> GetManufacturer()
-            {
-                using (var ctx = new ApplicationDbContext())
-                {
-                    var query = ctx.Manufacturers.Select(
-                        e =>
-                        new Manufacturer
-                        {
-                            Id = e.Id,
-                            CompanyName = e.CompanyName,
-                            Locations = e.Locations,
-                            Cars = e.Cars,
-                            Founded = e.Founded
-                        }
-                        );
-                    return query.ToList();
-                }
-            }
-
-           
-        
+        }
 
     }
 }
+
+
+
+
+
--- SpooderManCars.WebApi/Controllers/ManufacturerController.cs
+++ SpooderManCars.WebApi/Controllers/ManufacturerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using SpooderManCars.Data;
+using SpooderManCars.Models.ManufacturerModels;
 using SpooderManCars.Services;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Web.Http;
 using static SpooderManCars.Services.ManufacturerService;
@@ -42,6 +44,35 @@ namespace SpooderManCars.WebApi.Controllers
 
             return Ok();
         }
+
+        public IHttpActionResult Get(int id)
+        {
+            ManufacturerService manufacturerService = CreateManufacturerService();
+            var manufacturer = manufacturerService.GetManufacturerById(id);
+            return Ok(manufacturer);
+        }
+
+        public IHttpActionResult Put(ManufacturerEdit manufacturer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateManufacturerService();
+
+            if (!service.UpdateManufacturer(manufacturer))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete (int id)
+        {
+            var service = CreateManufacturerService();
+            if (!service.DeleteManufacturer(id))
+                return InternalServerError();
+            return Ok();
+        }
+
+        
 
        
     }
